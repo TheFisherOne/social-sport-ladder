372bb21eb8be8511d057637e421babec274e047d
TheFisherOne <douglasfisher99@gmail.com>
2025-06-20 12:39:03 -0600

    getting android build gradle working

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.social_sport_ladder"
+    namespace = "app.web.social_sport_ladder.rg"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.social_sport_ladder"
+        applicationId = "app.web.social_sport_ladder.rg"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,15 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.10.0")
-    }
+plugins {
+    // For your Android application module
+    id("com.android.application") version "8.10.0" apply false // Apply the AGP for app modules
+
+    // If you have Android library modules, add this too:
+    // id("com.android.library") version "8.10.0" apply false
+
+    // If you're using Kotlin for Android development (which is highly likely)
+    // Make sure to include the Kotlin Android plugin. Find its appropriate version.
+    // Example: (Check the latest compatible version for AGP 8.10.0)
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false // Or your current Kotlin version
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
